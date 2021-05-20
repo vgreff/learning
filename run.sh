f8a3f85b8c0326e925f04afda08e2b71117ac1ab
Vincent Greff <vince@greffsite.com>
2021-05-19 21:34:37 -0500

    vim airline

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 
 sudo yum install -y tig
 
+git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
+ln -s `pwd`/.vimrc ~/.vimrc
+
 echo "source `pwd`/bash_aliases" >> ~/.bashrc
 
 . ~/.bashrc
