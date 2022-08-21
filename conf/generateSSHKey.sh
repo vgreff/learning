aac40c18617ae31eb57ae037732944352e400645
vincent.greff <vince@greffsite.com>
2022-08-20 19:38:21 -0500

    install packages

---
+++ conf/generateSSHKey.sh
@@ -0,0 +1,7 @@
+cd
+ssh-keygen -t rsa -b 4096 -C "vince@greffsite.com"
+
+ls -la .ssh/
+
+cat .ssh/id_rsa.pub
+
---
+++ conf/install_packages.sh
@@ -0,0 +1,31 @@
+ sudo $YUM install -y git
+
+
+
+if [ -z `which apt`  ]
+then
+#------------------------------------------
+	echo "Redhat"
+
+echo "install code"
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+yum check-update
+sudo yum install code
+
+else
+#------------------------------------------
+	echo "Ubuntu"
+echo "install code"
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
+fi
