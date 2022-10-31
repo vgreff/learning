3fcb6995bb46fa1dd638be26b8a428492cb56333
vincent.greff <vince@greffsite.com>
2022-10-31 10:17:07 -0500

    imData

--- cpp/IntervalMap/main.cpp
+++ cpp/IntervalMap/main.cpp
@@ -13,28 +13,28 @@ namespace vbg
 
 
 
-template <typename T>
-class ImKey
+template <typename T, typename DATA>
+class ImData
 {
 public:
-  using  SPtr = std::shared_ptr<ImKey<T>>;
-  using  UPtr = std::unique_ptr<ImKey<T>>;
+  using  SPtr = std::shared_ptr<ImData<T, DATA>>;
+  using  UPtr = std::unique_ptr<ImData<T, DATA>>;
 
   using  Ptr = SPtr;
   template<class... Args >
-  static Ptr make( Args&&... args ) { return std::make_shared<ImKey<T>>(args ...); }
+  static Ptr make( Args&&... args ) { return std::make_shared<ImData<T, DATA>>(args ...); }
 
-  using  Vector = std::vector<ImKey<T>::Ptr>;
+  using  Vector = std::vector<ImData<T, DATA>::Ptr>;
 public:
-  ImKey(T startVal, T endVal);
+  ImData(T startVal, T endVal, DATA data);
 
-  inline friend bool operator==(const ImKey<T>& lhs, const ImKey<T>& rhs)
+  inline friend bool operator==(const ImData<T, DATA>& lhs, const ImData<T, DATA>& rhs)
   {
       // compares startVal_ to rhs.startVal_,
       // then endVal_.endVal_     // then d to rhs.d
       return std::tie(lhs.startVal_, lhs.endVal_) == std::tie(rhs.startVal_, rhs.endVal_);
   }
-  inline bool equalStart(const ImKey<T>& rhs) const
+  inline bool equalStart(const ImData<T, DATA>& rhs) const
   {
       return startVal_ == rhs.startVal_;
   }
@@ -56,15 +56,15 @@ public:
       return h1;
   }
 
-   friend std::ostream& operator<< <>(std::ostream& out, const ImKey<T>& obj);
+   friend std::ostream& operator<< <>(std::ostream& out, const ImData<T, DATA>& obj);
 private:
-  T startVal_{0};
-  T endVal_{0};
+  T startVal_;
+  T endVal_;
+  DATA data_;
 };
-
-template <typename T>
-ImKey<T>::ImKey(T startVal, T endVal)
-: startVal_(startVal), endVal_(endVal)
+template <typename T, typename DATA>
+ImData<T, DATA>::ImData(T startVal, T endVal, DATA data)
+: startVal_(startVal), endVal_(endVal), data_(data)
 {
 }
 
@@ -87,7 +87,7 @@ public:
 
   void add(const Key& keyStart, const Key& keyEnd, const T& val)
   {
-    ImKey<Key> myKey(keyStart, keyEnd);
+    ImData<Key, T> myKey(keyStart, keyEnd, val);
     fakeTotest_.insert_or_assign(myKey, val);
     directHit_.insert_or_assign(keyStart, val);
   }
@@ -96,7 +96,7 @@ public:
 private:
   T notFoundVal_;
   std::unordered_map<Key, T > directHit_;
-  std::unordered_map<ImKey<Key>, T > fakeTotest_;
+  std::unordered_map<ImData<Key, T>, T > fakeTotest_;
 };
 
 template <typename Key, typename T>
@@ -113,31 +113,28 @@ std::ostream& operator<<(std::ostream& out, const IntervalMap<Key, T>& obj)
      out << "}";
      return out;
 }
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const ImKey<T>& obj)
+template <typename T, typename DATA>
+std::ostream& operator<<(std::ostream& out, const ImData<T, DATA>& obj)
 {
-  out << "ImKey:{ ";
+  out << "ImData:{ ";
   out << "startVal=" << obj.startVal_ << ", "
       << "endVal=" << obj.endVal_ << " ";
   out << "}";
   return out;
 }
 } // namespace vbg
-
-template <typename T>
-struct std::hash<vbg::ImKey<T>>
+template <typename T, typename DATA>
+struct std::hash<vbg::ImData<T, DATA>>
 {
-    std::size_t operator()(const vbg::ImKey<T>& s) const noexcept
+    std::size_t operator()(const vbg::ImData<T, DATA>& s) const noexcept
     {
       return s.hashStart();
     }
 };
-
-template <typename T>
-struct std::equal_to<vbg::ImKey<T>>
+template <typename T, typename DATA>
+struct std::equal_to<vbg::ImData<T, DATA>>
 {
-    std::size_t operator()(const vbg::ImKey<T>& lhs, const vbg::ImKey<T>& rhs) const noexcept
+    std::size_t operator()(const vbg::ImData<T, DATA>& lhs, const vbg::ImData<T, DATA>& rhs) const noexcept
     {
       return lhs.equalStart(rhs);
     }
@@ -152,7 +149,7 @@ int main()
   vbg::MyIntervalMap intervalMap;
   std::cout << "MyIntervalMap=" << intervalMap << "\n";
 
-  ImKey<int> key(10,20);
+  ImData<int, char> key(10,20, 'D');
   std::cout << "key=" << key << "\n";
   
 
