15dca57b35423d2e4efab7b26a6621b63e923a4b
vincent.greff <vince@greffsite.com>
2022-11-09 12:25:39 -0500

    debugPid

--- cpp/DebugPid/main.cpp
+++ cpp/DebugPid/main.cpp
@@ -8,6 +8,15 @@ int main()
   std::cout << "This is main" << std::endl;
   vbg::MyDebugPid debugPid;
   std::cout << "debugPid=" << debugPid << "\n";
+  int64_t counter(0);
+  while (1)
+  {
+    if (counter % 1000000000 == 0)
+    {
+      std::cout << "counter=" << counter << "\n";
+    }
+    ++counter;
+  }
   
 
   return 0;
