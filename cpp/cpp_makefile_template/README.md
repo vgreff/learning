dc914bcd029b2cd125616e4f627852c08be7bbfa
vincent.greff <vince@greffsite.com>
2023-04-05 01:04:27 -0400

    makefile project config with vscode

---
+++ cpp/cpp_makefile_template/README.md
@@ -0,0 +1,6 @@
+select build target: debug
+launch traget program
+
+use makefile config from  launch.json for debugging
+
+look at .vscode/settings.json for configuration
--- cpp/cpp_makefile_template/src/program.cpp
+++ cpp/cpp_makefile_template/src/program.cpp
@@ -4,8 +4,20 @@
 #include <module2/mod2c2.hpp>
 #include <program.hpp>
 
-int main()
+#include<vector>
+
+int main(int argc, char const *argv[])
 {
+   if (argc < 2)
+   return 1;
+
+   printf("argv[1]=%s %d\n",argv[1], argc);
+
+   std::vector<int> v;
+   v.push_back(12);
+   v.push_back(120);
+   v.push_back(102);
+
    mod1c1 m1c1;
    mod1c2 m1c2;
    mod2c1 m2c1;
