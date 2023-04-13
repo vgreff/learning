2937f321182521a04ab64af9315200a0a344e03c
vincent.greff <vince@greffsite.com>
2023-04-13 00:06:43 -0400

    add vector

--- cpp/cpp_cmake_template/src/program.cpp
+++ cpp/cpp_cmake_template/src/program.cpp
@@ -4,8 +4,27 @@
 #include <module2/mod2c2.hpp>
 #include <program.hpp>
 
-int main()
+#include <vector>
+
+int main(int argc, char const *argv[])
 {
+   if (argc > 1)
+   {
+      for (int i = 0; i < argc; i++)
+      {
+         printf("argv[%d]=%s %d\n", i, argv[i], argc);
+      }
+   }
+   else
+   {
+      printf("argv[0]=%s %d\n", argv[0], argc);
+   }
+
+   std::vector<int> v;
+   v.push_back(12);
+   v.push_back(120);
+   v.push_back(102);
+
    mod1c1 m1c1;
    mod1c2 m1c2;
    mod2c1 m2c1;
