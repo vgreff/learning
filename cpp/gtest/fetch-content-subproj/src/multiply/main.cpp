5765396177d7d4cd7f45a98e67473902e9eaebaf
vincent.greff <vince@greffsite.com>
2024-02-05 23:29:15 -0500

    testnate subproj

---
+++ cpp/gtest/fetch-content-subproj/src/multiply/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <multiply/multiply.h>
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+
+    std::cout  << multiply(2.5, 3.0)  << std::endl;
+    
+    return 0;
+}
